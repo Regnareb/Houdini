8687ba6f4b4a0b5bbbad3f14c81ba8da16559387
Regnareb <kun_fu_man@hotmail.com>
2023-06-30 22:17:42 +0200

    Force the user desktop when opening a scene

--- scripts/123.py
+++ scripts/123.py
@@ -1,5 +1,9 @@
-﻿
-import hou
+﻿import hou
 
-#keepfloatwindowsontop
 hou.setUpdateMode(hou.updateMode.Manual)
+
+# Force Desktop
+for i in hou.ui.desktops():
+    if i.name() == hou.getPreference('general.desk.val'):
+        i.setAsCurrent()
+        break

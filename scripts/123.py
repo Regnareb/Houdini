48dcd8df38e7ef1b5c6d508e1a659daa9eaad22c
Regnareb <kun_fu_man@hotmail.com>
2023-07-10 17:13:03 +0200

    Remove 123.py file

--- scripts/123.py
+++
@@ -1,9 +0,0 @@
-﻿import hou
-
-hou.setUpdateMode(hou.updateMode.Manual)
-
-# Force Desktop
-for i in hou.ui.desktops():
-    if i.name() == hou.getPreference('general.desk.val'):
-        i.setAsCurrent()
-        break
--- scripts/456.py
+++ scripts/456.py
@@ -1,1 +1,10 @@
-123.py
+﻿import hou
+
+if hou.getPreference('custom.regnareb.on_open_go_manual'):
+    hou.setUpdateMode(hou.updateMode.Manual)
+
+# Force Desktop
+for i in hou.ui.desktops():
+    if i.name() == hou.getPreference('general.desk.val'):
+        i.setAsCurrent()
+        break

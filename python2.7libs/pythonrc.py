9ca423a5694ff28c7e4dc8557af1cd6815b05306
Regnareb <kun_fu_man@hotmail.com>
2026-07-30 22:10:15 +0200

    Move 456.py code to pythonrc.py for better compatibility with studios

--- python2.7libs/pythonrc.py
+++ python2.7libs/pythonrc.py
@@ -1,5 +1,20 @@
+import hou
 import hdefereval
+import common
+import common.hou_utils
+import common.sceneviewer
 import common.preferences as prefs
 
+def scene_event_callback(event_type):
+    if event_type in [hou.hipFileEventType.BeforeLoad, hou.hipFileEventType.BeforeMerge]:
+        if hou.getPreference('custom.regnareb.on_open_go_manual'):
+            common.hou_utils.toggle_update_mode(hou.updateMode.Manual)
+        if hou.getPreference('custom.regnareb.on_open_sopviewmode'):
+            hou.setPreference('tools.sopviewmode.val', '0')
+        if hou.getPreference('custom.regnareb.on_open_change_desktop'):
+            hou.ui.desktop(hou.getPreference('general.desk.val')).setAsCurrent()
+        # if hou.getPreference('custom.regnareb.on_open_hide_other_objects'):
+        #     common.sceneviewer.hide_other_objects()
 
 hdefereval.executeDeferred(prefs.show_firstlaunch)
+hou.hipFile.addEventCallback(scene_event_callback)

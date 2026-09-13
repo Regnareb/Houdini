b1a0bd760ae99df6b7c4363d0c57f5d9e76d6b08
Regnareb <kun_fu_man@hotmail.com>
2026-09-13 02:20:13 +0200

    Do not save the "key animated badge" background image in the file anymore. The badge is now reapplied when opening a file.
    It now delete every keyed badges before saving the file to prevent them being pushed into the pipeline to other users.
    It applies them back when loading a scene, after a save and a file merge.

--- scripts/python/common/events.py
+++ scripts/python/common/events.py
@@ -1,4 +1,6 @@
+import re
 import hou
+import nodegraphutils
 import common.networkeditor
 
 
@@ -25,6 +27,7 @@ def event_color_changed(node, event_type=None, **kwargs):
 
 
 def event_is_animated(node, event_type=None, **kwargs):
+    # Bug some background images appear unconnected to a node
     # If you delete the node and undo, the callbacks are deleted and it won't reinstate the behaviour unless you reload the scene
     def delete_background_image(node, **kwargs):
         try:
@@ -36,7 +39,6 @@ def event_is_animated(node, event_type=None, **kwargs):
     children = node.children()
     node_time_dependent = any([parm.isTimeDependent() for parm in node.parms()])
     children_time_dependent = any([parm.isTimeDependent() for node in children for parm in node.parms()])
-    editor = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
     if not any((node_time_dependent, children_time_dependent)):
         delete_background_image(node)
         return
@@ -46,5 +48,25 @@ def event_is_animated(node, event_type=None, **kwargs):
         filepath = '$REGNAREB/images/keyed_node.png'
     elif children_time_dependent:
         filepath = '$REGNAREB/images/keyed_children.png'
-    common.networkeditor.add_background_image(editor, hou.text.expandString(filepath), rect=None, node=node, relative=True, width_ratio=0.6, stick_to_side='right', offset=hou.Vector2(0.3, -0.3))
+    common.networkeditor.add_background_image(node, hou.text.expandString(filepath), rect=None, relative=True, width_ratio=0.7, stick_to_side='right', offset=hou.Vector2(0.1, -0.55))
     node.addEventCallback((hou.nodeEventType.BeingDeleted,), delete_background_image)
+
+
+def event_clear_all_animated(event_type=None, **kwargs):
+    nodes = hou.node("/").allSubChildren()
+    for node in nodes:
+        images = nodegraphutils.loadBackgroundImages(node)
+        if images:
+            images = [i for i in images if not re.search('keyed_(children|node|both)\.png$', i.path())]
+            nodegraphutils.saveBackgroundImages(node, images)
+
+
+def event_add_all_animated(event_type=None, **kwargs):
+    nodes = hou.node("/").allSubChildren()
+    for node in nodes:
+        event_is_animated(node)
+
+
+# import common.events
+# common.events.event_clear_all_animated()
+# common.events.event_add_all_animated()
--- scripts/python/ready.py
+++ scripts/python/ready.py
@@ -13,6 +13,10 @@ def scene_event_callback(event_type):
             hou.ui.desktop(hou.getPreference('general.desk.val')).setAsCurrent()
         # if hou.getPreference('custom.regnareb.on_open_hide_other_objects'):
         #     common.sceneviewer.hide_other_objects()
+    if event_type in [hou.hipFileEventType.AfterLoad, hou.hipFileEventType.AfterSave, hou.hipFileEventType.AfterMerge]:
+        hdefereval.executeDeferred(common.events.event_add_all_animated)
+    if event_type == hou.hipFileEventType.BeforeSave:
+        common.events.event_clear_all_animated()
 
 hdefereval.executeDeferred(prefs.show_firstlaunch)
 hou.hipFile.addEventCallback(scene_event_callback)
